126140ff667b58e864efdf3b7b5fc10b3aecf1a8
Federico Frigerio <federico.frigerio@studenti.unitn.it>
2023-04-17 23:55:35 +0200

    Correzioni minori alla cache di autenticazione

--- ExpiredKeyRemover/Config.cs
+++ ExpiredKeyRemover/Config.cs
@@ -5,5 +5,5 @@ public class Config
 	public string ExpiredChannelName { get; set; } = "__keyevent@0__:expired";
 	public string UsersHashKey { get; set; } = "users";
 	public uint ExpiredScanSleepSeconds { get; set; } = 5;
-	public uint DandlingScanSleepMinutes { get; set; } = 20;
+	public uint DandlingScanSleepMinutes { get; set; } = 5;
 }
