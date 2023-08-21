f47b3145a6045b403a7f8e0de0ae6ddef644af6b
Federico Frigerio <federico.frigerio@studenti.unitn.it>
2023-08-21 13:09:33 +0200

    Minificati i template email e piccole modifiche agli user service

--- Shared/Models/DefaultPermissions.cs
+++ Shared/Models/DefaultPermissions.cs
@@ -2,15 +2,12 @@ namespace Shared.Models;
 
 public struct DefaultPermissions
 {
-    public const uint CreateChats = 1;
-    public const uint CreateGroups = 2;
-    public const uint ManageUsersAccounts = 4;
-    public const uint EnableLocal2Fa = 8;
-    public const uint ResetUsersPassword = 16;
-    public const uint BlockUsers = 32;
-    public const uint EnforceGlobal2Fa = 64;
-    public const uint ForcePasswordChange = 128;
-    public const uint ForceGlobalPasswordChange = 256;
-    public const uint ReadOtherUsersChats = 512;
-    public const uint DeleteSupervisedChat = 1024;
+	public const uint CreateChats = 1;
+	public const uint CreateGroups = 2;
+	public const uint ManageUsersAccounts = 4;
+	public const uint EnableLocal2Fa = 8;
+	public const uint BlockUsers = 16;
+	public const uint EnforceGlobal2Fa = 32;
+	public const uint ReadOtherUsersChats = 64;
+	public const uint DeleteSupervisedChat = 128;
 }
