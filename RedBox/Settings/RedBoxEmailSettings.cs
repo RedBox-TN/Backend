881be8bdc5e81d5266ff4c5357b5a8903452960b
lorysmutti <lorys.mutti@studenti.unitn.it>
2023-08-26 18:19:18 +0200

    Added password change API from logged user Added email notification for password change Added html template for password changed Adjusted fetch API for multiple identifiers

--- RedBox/Settings/RedBoxEmailSettings.cs
+++ RedBox/Settings/RedBoxEmailSettings.cs
@@ -5,6 +5,7 @@ public class RedBoxEmailSettings
 	public string ApplicationUrl { get; set; } = null!;
 	public string AccountCreationTemplateFile { get; set; } = null!;
 	public string EmailConfirmUrl { get; set; } = null!;
-	public string EmailConfirmTemplate { get; set; } = null!;
+	public string EmailConfirmTemplateFile { get; set; } = null!;
 	public string NewPasswordTemplateFile { get; set; } = null!;
+	public string ChangedPasswordTemplateFile { get; set; } = null!;
 }
