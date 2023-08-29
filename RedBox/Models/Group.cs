0b24a8208b17a511751a4e728f0a1065c822b175
Federico Frigerio <federico.frigerio@studenti.unitn.it>
2023-08-29 12:25:33 +0200

    Aggionamento delle dipendenze

--- RedBox/Models/Group.cs
+++ RedBox/Models/Group.cs
@@ -2,6 +2,7 @@ namespace RedBox.Models;
 
 public sealed class Group : Chat
 {
+	public string? GroupPicId { get; set; } = null!;
 	public string? Name { get; set; } = null!;
 	public string[]? AdminsIds { get; set; } = null!;
 }
