5c96e51832c2dd8e1e095300238cdb8f7ad34f28
FrostWalk <federico.frigerio@studenti.unitn.it>
2023-12-22 23:37:59 +0100

    Aggiunta notifica di accesso

--- RedBox/Email utility/IRedBoxEmailUtility.cs
+++ RedBox/Email utility/IRedBoxEmailUtility.cs
@@ -6,6 +6,5 @@ public interface IRedBoxEmailUtility
 	public Task SendAccountCreationAsync(string address, string username, string name, string password);
 	public Task SendEmailChangedAsync(string address, string id, string username);
 	public Task SendAdminPasswordChangedAsync(string address, string password, string username);
-
 	public Task SendPasswordChangedAsync(string address, string username);
 }
--- RedBoxAuth/Email utility/IAuthEmailUtility.cs
+++ RedBoxAuth/Email utility/IAuthEmailUtility.cs
@@ -4,4 +4,5 @@ public interface IAuthEmailUtility
 {
 	public Task SendPasswordResetRequestAsync(string address, string username, string id);
 	public Task SendAccountLockNotificationAsync(string address, string username);
+	public Task SendLoginNotificationAsync(string username, string ip, string userAgent, DateTime timeStamp);
 }
